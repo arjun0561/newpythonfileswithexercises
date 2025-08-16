64bb6392eb6b0d919548bc84d662caf6f1b9813c
ARJUN CHALLA <78394236+arjun0561@users.noreply.github.com>
2025-08-16 12:24:15 -0500

    Update password_generator.py

--- python_exercises/Games/password_generator.py
+++ python_exercises/Games/password_generator.py
@@ -1,20 +1,3 @@
-# import random
-
-# def password_generator():
-#     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-#     symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", '[', ']', "{", "}", "-", "_", "+", "=", "|", "?", "/", ":", ";", "<", ">", ",", "."]
-#     uppercase_chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", 'Z']
-#     lowercase_chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", 'z']
-#     random_password = random.choice(numbers, symbols, uppercase_chars, lowercase_chars)
-#     if len(random_password) <= 8:
-#         return "Bad Password"
-#     elif len(random_password) > 8:
-#         return "Good password"
-    
-#     print(random_password)
-
-# password_generator()
-
 import random
 import string
 
